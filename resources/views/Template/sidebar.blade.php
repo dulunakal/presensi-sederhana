bcff0de889a8c49956e39fd2343544fa9653706f
dulunakal <dulunakal@MacBook-Air-dulunakal.local>
2021-06-20 13:18:39 +0700

    Menambahkan Middleware

--- resources/views/Template/sidebar.blade.php
+++ resources/views/Template/sidebar.blade.php
@@ -32,6 +32,7 @@
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
              with font-awesome or any other icon font library -->
+        @if (auth()->user()->level =="karyawan")
         <li class="nav-item has-treeview">
           <a href="#" class="nav-link">
             <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -55,7 +56,10 @@
             </li>
           </ul>
         </li>
+        @endif
+            @if (auth()->user()->level =="karyawan")
         <li class="nav-item has-treeview">
+            @endif
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
@@ -64,19 +68,31 @@
               </p>
             </a>
             <ul class="nav nav-treeview">
+            @if (auth()->user()->level =="karyawan")
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Laporan Per Karyawan</p>
                 </a>
               </li>
+            @endif
+            @if (auth()->user()->level =="admin")
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Laporan Keseluruhan</p>
                 </a>
               </li>
+            @endif
             </ul>
+          </li>
+          <li class="nav-item">
+              <a href="{{route('logout')}}" class="nav-link">
+                  <i class="nav-icon fas fa-th"></i>
+                  <p>
+                      Logout
+                  </p>
+              </a>
           </li>
       </ul>
     </nav>
